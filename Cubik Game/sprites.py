5a8ac26487842feacdad64da8c39b6c7c400da06
OleksandrKasapchuk <105971884+OleksandrKasapchuk@users.noreply.github.com>
2023-11-13 22:38:38 +0200

    Update sprites.py

--- Cubik Game/sprites.py
+++ Cubik Game/sprites.py
@@ -3,6 +3,7 @@ from constants import *
 from map import * 
 from logger import *
 
+
 class GameSprite(sprite.Sprite):
     def __init__ (self, player_image, player_speed, player_x, player_y, size_x, size_y, direction):
         super().__init__()
@@ -12,6 +13,7 @@ class GameSprite(sprite.Sprite):
         self.rect.x = player_x
         self.rect.y = player_y
         self.direction = direction
+
 
     def reset(self):
         WINDOW.blit(self.image, (self.rect.x, self.rect.y))
@@ -38,14 +40,20 @@ class Player(sprite.Sprite):
         self.index = 0
         self.counter = 0
         self.direction = 0
+        self.bullets = sprite.Group()
+        self.last_shot_time = -5000  # Змінна для відстеження часу останнього вистрілу
+        self.shoot_cooldown = 5000  # Затримка між вистрілами у мілісекундах
 
     def reset(self): 
         WINDOW.blit(self.image, (self.rect.x, self.rect.y))
+        self.bullets.update()
+        self.bullets.draw(WINDOW)
 
     def update(self):
         dx = 0
         dy = 0
         walk_cooldown = 1
+        current_time = time.get_ticks()
         keys = key.get_pressed() 
         if keys[K_d] or keys[K_RIGHT] and self.rect.x < 780: 
             dx = self.speed
@@ -63,17 +71,22 @@ class Player(sprite.Sprite):
             dy = self.speed
             self.counter += 1
             self.direction = -2
+        elif keys[K_SPACE]:# and current_time - self.last_shot_time > self.shoot_cooldown:
+            self.fire()
+            self.last_shot_time = current_time
+
+        for bullet in self.bullets:
+            bullet.update()
 
         for tile in world.tile_list:
             #check for collision in x direction
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.size_x, self.size_y):
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.size_x, self.size_y) and tile not in bushes:
                 dx = 0
                 
             #check for collision in y direction
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.size_x, self.size_y):
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.size_x, self.size_y) and tile not in bushes:
                 dy = 0
 
-            
         if self.counter > walk_cooldown:
             self.counter = 0	
             self.index += 1
@@ -93,11 +106,26 @@ class Player(sprite.Sprite):
         self.rect.y += dy
         
 
+    def fire(self):
+        bullet = Bullet("assets/images/bullet.png", 10, self.rect.centerx, self.rect.top, 45, 45, self.direction)
+        self.bullets.add(bullet)
+
 class Enemy(GameSprite):
     def update(self):
         pass
 
 class Bullet(GameSprite):
+    def __init__(self, player_image, player_speed, player_x, player_y, size_x, size_y, direction):
+        super().__init__(player_image, player_speed, player_x, player_y, size_x, size_y, direction)
+
+        if direction in [1, -1]:
+            self.rotation = 270 if direction == 1 else 90
+            self.image = transform.flip(self.image, True, False)
+        elif direction in [2, -2]:
+            self.rotation = 0 if direction == 2 else 180
+
+        self.image = transform.rotate(self.image, self.rotation)
+
     def update(self):
         if self.direction == 1:
             self.rect.x += self.speed
@@ -107,8 +135,4 @@ class Bullet(GameSprite):
             self.rect.y -= self.speed
         elif self.direction == -2:
             self.rect.y += self.speed
-        '''
-        if self.rect.y > WIN_HEIGHT or self.rect.x > WIN_WIDTH or self.rect.y < 0 or self.rect.x < 0:
-            self.kill()
-            fired = False
-        '''
+
