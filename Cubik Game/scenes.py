9e3694a469e85578e4c7b5e2f39a6ddf30f454c9
OleksandrKasapchuk <105971884+OleksandrKasapchuk@users.noreply.github.com>
2023-11-17 20:06:50 +0200

    Update scenes.py

--- Cubik Game/scenes.py
+++ Cubik Game/scenes.py
@@ -1,3 +1,4 @@
+#імпортуємо файли
 from constants import *
 from abc import ABC, abstractmethod
 from pygame import *
@@ -5,12 +6,13 @@ from map import *
 from sprites import *
 from logger import *
 
+#асбтрактний клас сцени
 class Scene(ABC):
     @abstractmethod
     def update(self):
         pass
 
-# Оновлений клас GameScene
+#клас сцени гри
 class GameScene(Scene):
     def __init__(self):
         self.main_character = player
@@ -22,9 +24,10 @@ class GameScene(Scene):
         self.tanks_group = sprite.Group(self.fast_tank, self.armored_tank)
         self.heart_image_red = transform.scale(image.load("assets/images/heart_red.png"), (20, 20))
         self.heart_image_white = transform.scale(image.load("assets/images/heart_white.png"), (20, 20))
-
+    #оновлення сцени гри
     def update(self):
         WINDOW.fill(BLACK_COLOR)
+        #промальовка персонажів
         self.main_character.reset()
         self.main_character.update()
         self.world.draw()
@@ -33,12 +36,13 @@ class GameScene(Scene):
         for tank in self.tanks_group:
             tank.reset()
             tank.update()
+            
 
         current_health = self.main_character.health
         max_health = self.main_character.max_health
 
         heart_width = 20
-
+        #показ життів
         for i in range(max_health):
             heart_x = WIN_WIDTH - 10 - (i + 1) * (heart_width + 20)
             heart_y = 10
@@ -58,5 +62,5 @@ class GameScene(Scene):
     def on_lose_game(self):
         print("Game Over. You lost!")
 
-# Створено об'єкт GameScene
+#об'єкт сцени
 game = GameScene()
