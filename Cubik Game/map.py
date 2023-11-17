a4b1edbf41655c06a5093c4679ceccd3ee42709e
OleksandrKasapchuk <105971884+OleksandrKasapchuk@users.noreply.github.com>
2023-11-17 20:06:36 +0200

    Update map.py

--- Cubik Game/map.py
+++ Cubik Game/map.py
@@ -1,29 +1,31 @@
+#імпортуємо модулі
 from pygame import *
 from constants import *
 from logger import *
 
 tile_size = 64
-
+#списки блоків
 bushes = []
 bricks = []
 touchabels = []
-
+#клас
 class World():
     def __init__(self, data):
         self.tile_list = []
 
         #load images
-        brick_img = image.load("assets/images/brick.jpg")
-        beton_img = image.load("assets/images/beton.jpg")
-        ice_img = image.load("assets/images/ice.jpg")
+        brick_img = image.load("assets/images/brick.png")
+        beton_img = image.load("assets/images/beton.png")
+        ice_img = image.load("assets/images/ice.png")
         bush_img = image.load("assets/images/bush.png")
-        water_img = image.load("assets/images/water.jpg")
+        water_img = image.load("assets/images/water.png")
         
         row_count = 0
         
         for row in data:
             col_count = 0
             for tile in row:
+                #блок цегли
                 if tile == 1:
                     img = transform.scale(brick_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
@@ -33,7 +35,7 @@ class World():
                     self.tile_list.append(tile)
                     bricks.append(tile)
                     touchabels.append(tile)
-
+                #блок бетону
                 elif tile == 2:
                     img = transform.scale(beton_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
@@ -42,7 +44,7 @@ class World():
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                     touchabels.append(tile)
-
+                #блок куща
                 elif tile == 3:
                     img = transform.scale(bush_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
@@ -51,15 +53,7 @@ class World():
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                     bushes.append(tile)
-
-                elif tile == 4:
-                    img = transform.scale(ice_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-
+                #блок води
                 elif tile == 5:
                     img = transform.scale(water_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
@@ -75,19 +69,19 @@ class World():
     def draw(self):
         for tile in self.tile_list:
             WINDOW.blit(tile[0], tile[1])
-
+#мапа
 map1 = [[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
         [0, 1, 1, 2, 2, 0, 0, 0, 0, 1, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 3, 0, 0, 0, 0, 0, 0],
-        [0, 0, 4, 4, 3, 3, 3, 3, 0, 0, 0, 1, 0],
-        [0, 0, 4, 5, 5, 3, 1, 1, 0, 4, 4, 2, 0],
+        [1, 2, 0, 0, 3, 3, 3, 3, 0, 0, 0, 1, 0],
+        [0, 0, 0, 5, 5, 3, 1, 1, 0, 3, 3, 2, 0],
         [1, 0, 0, 5, 5, 3, 3, 1, 0, 5, 5, 2, 0],
-        [1, 0, 1, 2, 1, 3, 3, 0, 4, 5, 5, 3, 0],
-        [2, 0, 0, 0, 0, 0, 0, 0, 4, 3, 3, 3, 0],
+        [1, 0, 1, 2, 1, 3, 3, 0, 0, 5, 5, 3, 0],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
         [2, 0, 0, 1, 1, 0, 2, 0, 3, 3, 3, 0, 0],
-        [0, 0, 2, 2, 1, 0, 0, 0, 3, 2, 1, 0, 0],
+        [0, 0, 2, 2, 1, 0, 0, 0, 2, 2, 1, 0, 0],
         [0, 1, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
         [0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1],
         [0, 0, 0, 0, 0, 1, 6, 1, 0, 0, 0, 1, 1]]
-
+#екземпляр світу
 world = World(map1)
