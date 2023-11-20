6ecf6d1b4158d8662da152de4a8133d027140c03
OleksandrKasapchuk <105971884+OleksandrKasapchuk@users.noreply.github.com>
2023-11-20 23:17:04 +0200

    Update map.py

--- Cubik Game/map.py
+++ Cubik Game/map.py
@@ -3,25 +3,18 @@ from pygame import *
 from constants import *
 from logger import *
 
-tile_size = 64
 #списки блоків
 bushes = []
 bricks = []
 touchabels = []
+
 #клас
 class World():
     def __init__(self, data):
         self.tile_list = []
-
-        #load images
-        brick_img = image.load("assets/images/brick.png")
-        beton_img = image.load("assets/images/beton.png")
-        ice_img = image.load("assets/images/ice.png")
-        bush_img = image.load("assets/images/bush.png")
-        water_img = image.load("assets/images/water.png")
         
         row_count = 0
-        
+       
         for row in data:
             col_count = 0
             for tile in row:
@@ -31,7 +24,7 @@ class World():
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    tile = [img, img_rect, max_health]
                     self.tile_list.append(tile)
                     bricks.append(tile)
                     touchabels.append(tile)
@@ -70,11 +63,11 @@ class World():
         for tile in self.tile_list:
             WINDOW.blit(tile[0], tile[1])
 #мапа
-map1 = [[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-        [0, 1, 1, 2, 2, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 3, 0, 0, 0, 0, 0, 0],
+map1 = [[0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 0, 0, 0],
+        [0, 1, 1, 2, 2, 0, 0, 0, 0, 2, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 3, 0, 0, 0, 0, 2, 0],
         [1, 2, 0, 0, 3, 3, 3, 3, 0, 0, 0, 1, 0],
-        [0, 0, 0, 5, 5, 3, 1, 1, 0, 3, 3, 2, 0],
+        [0, 0, 0, 5, 5, 3, 1, 1, 0, 3, 3, 1, 0],
         [1, 0, 0, 5, 5, 3, 3, 1, 0, 5, 5, 2, 0],
         [1, 0, 1, 2, 1, 3, 3, 0, 0, 5, 5, 3, 0],
         [2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
